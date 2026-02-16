b90fafcb5a69c683a3a0afb31a90dc2734d49476
DenisLadchenkov <119222041+Dentapinos@users.noreply.github.com>
2026-02-16 11:09:15 +0300

    init

---
+++ src/main/java/fr/mossaab/docmultilang/DocMultilangApplication.java
@@ -0,0 +1,13 @@
+package fr.mossaab.docmultilang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocMultilangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocMultilangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/mossaab/docmultilang/DocMultilangApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.mossaab.docmultilang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocMultilangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
